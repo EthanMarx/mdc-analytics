4aed823c4a1ad7a7054f7f75c4de7727134c4cfe
ethan.marx <ethan.marx@ligo.org>
2025-07-18 07:02:58 -0700

    update condor

--- executable.sh
+++ executable.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-cd /home/ethan.marx/projects/mdc-analytics
-uv run mdc-crossmatch --config configs/crossmatch/o3-mdc.yaml --max_workers 15 
+tar -xzf mdc-analytics_sandbox.tar.gz
+
+# Note: the condor.sub file should have the --config and --injection_file paths specified
+# using `transfer_input_files`, 
+apptainer exec --fakeroot mdc-analytics_sandbox/ mdc-crossmatch --config o3-mdc.yaml --injection_file o3-mdc-injections.hdf5 --outdir ./ --max_workers 15
